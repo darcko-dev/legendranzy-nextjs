7013b9363f0cad65051b1c716bb57928aa3abc5d
Zeta <80638821+darcko-dev@users.noreply.github.com>
2024-01-05 04:53:33 +0000

    feat: Add HeroSection component to quienes-somos page and update import of Hero component in quienes-somos page

--- src/app/quienes-somos/page.tsx
+++ src/app/quienes-somos/page.tsx
@@ -1,8 +1,14 @@
 import { Hero } from "@/components/hero/hero";
 export default function QuienesSomos() {
+  const HeroSection = [
+    {
+      title: "Quienes Somos",
+      content: "Información de quienes somos",
+    },
+  ];
   return (
     <main>
-      <Hero />
+      <Hero HeroSection={HeroSection} />
     </main>
   );
 }
